a9d62cccfdb53532432c883110f8de87d39363ab
badMeme <noveladdress@gmail.com>
2022-09-22 17:39:04 -0500

    testing heroku fixes

--- controllers/test_controller.js
+++ controllers/test_controller.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require ('bcrypt');
 const methodOverride = require('method-override');
 const axios = require('axios');
-const fetch = require('node-fetch');    //npm install node-fetch
+const fetcher = require('node-fetch');    //npm install node-fetch
 require('dotenv').config()
 
 const { REACT_APP_API_KEY } = process.env
@@ -52,27 +52,27 @@ router.get("/users", async (req,res)=>{
 })
 
 //Profile Show 
-router.get("/profile/:ext/", async (req, res) => {
+// router.get("/profile/:ext/", async (req, res) => {
 
-    const options = {
-        method: "GET",
-        headers: {
-          Authorization: REACT_APP_API_KEY,
-        },
-    }
+//     const options = {
+//         method: "GET",
+//         headers: {
+//           Authorization: REACT_APP_API_KEY,
+//         },
+//     }
 
-    try {
-        let [profile] = await Models.Player.find({gameName: req.params.ext})
-        console.log("user: " + profile)
-        data = await profile.json();
-        res.send(data)
+//     try {
+//         let [profile] = await Models.Player.find({gameName: req.params.ext})
+//         console.log("user: " + profile)
+//         data = await profile.json();
+//         res.send(data)
 
 
-    } catch (err) {
-        console.log(err)
-    }
+//     } catch (err) {
+//         console.log(err)
+//     }
 
-})
+// })
 
 // //Profile Show 
 // router.get("/profile/:ext/", async (req, res) => {
@@ -109,15 +109,12 @@ router.get("/profile/:ext/:tag", async (req, res) => {
     }
 
     try {
-        // let [profile] = await Models.Player.find({gameName: req.params.ext})
-        // console.log("user: " + profile)
         
-        //let real = await fetch(`https://api.henrikdev.xyz/valorant/v1/account/${profile.gameName}/${profile.tag}`)
-        let real = await fetch(`https://api.henrikdev.xyz/valorant/v1/account/${req.params.ext}/${req.params.tag}`, options)
+        let real = await fetcher(`https://api.henrikdev.xyz/valorant/v1/account/${req.params.ext}/${req.params.tag}`, options)
         let data = await real.json();
 
         //console.log("Fest test: ", apiData.data)
-        const real2 = await fetch(`https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/na/${data.data.puuid}`, options)
+        const real2 = await fetcher(`https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/na/${data.data.puuid}`, options)
         //console.log("Resonse 2: ", response2)
         const data2 = await real2.json();
 
